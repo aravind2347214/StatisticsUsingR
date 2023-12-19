dc0f7f5a34256f1ac23a287592994046fb8151e5
aravind2347214 <aravind.nandakumar@mca.christuniversity.in>
2023-12-19 15:54:56 +0530

    fghjk

--- Classwork/classwork.r
+++ Classwork/classwork.r
@@ -1,14 +1,14 @@
 v1<-c(1,2,TRUE,3,"hjknasfk",5);
 
 v3<-c(1,2,3,4)
-v2=seq(1:10)
+v2 = seq(1:10)
 typeof(v1)
 typeof(v2)
 typeof(v3)
 x<-c("Jan","Feb","March","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec")
-y=x[c(3,2,7)]
-z=x[c(TRUE,FALSE,FALSE,TRUE,TRUE,FALSE,FALSE,TRUE)]
-q=x[c(-2,-4)]
+y = x[c(3,2,7)]
+z = x[c(TRUE,FALSE,FALSE,TRUE,TRUE,FALSE,FALSE,TRUE)]
+q = x[c(-2,-4)]
 
 print("Index Based")
 print(y)
@@ -29,13 +29,13 @@ print(product)
 print(diffrence)
 print(quotient)
 
-sorted_a2 = sort(a2)
+sorted_a2  =  sort(a2)
 print(sorted_a2)
 
-rev_sort_a2 = sort(a2,decreasing = TRUE)
+rev_sort_a2  =  sort(a2,decreasing  =  TRUE)
 print(rev_sort_a2)
 
-order_a2=order(a2)
+order_a2 = order(a2)
 print(order_a2)
 
 
@@ -47,26 +47,26 @@ print(sum_of_sales)
 avg_sales<-sum_of_sales/length(sales_data)
 print(avg_sales)
 
-maxSales<-order(sales_data,decreasing = TRUE)[1]
+maxSales<-order(sales_data,decreasing  =  TRUE)[1]
 print(maxSales)
 
 minSales<-order(sales_data)[1]
 print(minSales)
 
-sales_data[3]=sales_data[3]+sales_data[3]/10
+sales_data[3] = sales_data[3]+sales_data[3]/10
 print(sales_data[3])
 print(sales_data)
 
 sorted_sales<-sort(sales_data)
-reverse_sales<-sort(sales_data,decreasing = TRUE)
+reverse_sales<-sort(sales_data,decreasing  =  TRUE)
 #------------------------------------------------------------------
 
 #creation of matrix1 using matrix function
-mat1<-matrix(c(10,5,3,6),nrow = 2,ncol=2,byrow = TRUE,dimnames = list(c("m1R1","m1R2"),c("m1C1","m1C2")))
+mat1<-matrix(c(10,5,3,6),nrow  =  2,ncol = 2,byrow  =  TRUE,dimnames  =  list(c("m1R1","m1R2"),c("m1C1","m1C2")))
 print(mat1)
 
 #creation of matrix2 using matrix function
-mat2<-matrix(c(1,4,8,7),nrow = 2,ncol=2,byrow = FALSE)
+mat2<-matrix(c(1,4,8,7),nrow  =  2,ncol = 2,byrow  =  FALSE)
 print(mat2)
 
 #class of matrix using matrix function
@@ -75,8 +75,8 @@ print(class(mat1))
 print("Class of Mat2 ")
 print(class(mat2))
 
-rownames(mat2)=c("m2R1","m2R2")
-colnames(mat2)=c("m2C1","m2C2")
+rownames(mat2) = c("m2R1","m2R2")
+colnames(mat2) = c("m2C1","m2C2")
 
 
 matSum<-mat1+mat2
@@ -139,7 +139,7 @@ print(mat1[,])
 print(changeDmat[c(TRUE,FALSE,TRUE),c(TRUE,TRUE,FALSE)])
 print(changeDmat[c(1,2),c(TRUE,TRUE,FALSE)])
 print(changeDmat[changeDmat>2])
-print(changeDmat[changeDmat%%2==0])
+print(changeDmat[changeDmat%%2 =  = 0])
 print(mat1[,"m1C1"])
 print(mat1["m1R1",])
 print(mat1["m1R2","m1C1"])
@@ -179,19 +179,19 @@ invMat1<-solve(mat1)
 print(invMat1)
 
 #creating identity matrix
-identityMat=matrix(c(1,0,0,1),nrow = 2,ncol = 2)
+identityMat = matrix(c(1,0,0,1),nrow  =  2,ncol  =  2)
 print(identityMat)
 
 #checking orthogonality
-mulmattran<-tranMat1==invMat1
-c=0
+mulmattran<-tranMat1 =  = invMat1
+c = 0
 for (x in mulmattran) {
-  if(x==FALSE){
-    c=1
+  if(x =  = FALSE){
+    c = 1
     break
   }
 }
-if(c==0){
+if(c =  = 0){
   print("It is orthogonal")
 }else{
   print("It is not orthogonal")
@@ -203,7 +203,7 @@ if(c==0){
 x1<-c("Dec","Jan","May","Nov")
 month_levels<-c("Jan","Feb","March","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec")
 
-y1<-factor(x1,levels=month_levels)
+y1<-factor(x1,levels = month_levels)
 print(y1)
 a<-c(1,2,3,4,5)
 b<-as.ordered(a)
@@ -217,7 +217,7 @@ is.ordered(b)
 #list
 list_info<-list("Blue","YEllow",c(12,3,24),TRUE,13.5,103.5);
 length(list_info)
-list_info[2]= NULL
+list_info[2] =  NULL
 length(list_info)
 print(list_info)
 
@@ -230,17 +230,17 @@ sizes
 sizes<-relevel(sizes,"Medium")
 sizes
 
-sizes<-factor(sizes,levels = rev(levels(sizes)))
+sizes<-factor(sizes,levels  =  rev(levels(sizes)))
 sizes
 
 #Dataframe 
 df<- data.frame(
-                NumberCol = 1:3,
-                TextcCol = c("FirstText", "SecondText","ThirdText"),
-                BoolCol = c(TRUE,TRUE,FALSE),
-                DoubleCol = c(1.2,4.4,7.4),
-                #stringsAsFactors = FALSE
-                stringsAsFactors = TRUE
+                NumberCol  =  1:3,
+                TextcCol  =  c("FirstText", "SecondText","ThirdText"),
+                BoolCol  =  c(TRUE,TRUE,FALSE),
+                DoubleCol  =  c(1.2,4.4,7.4),
+                #stringsAsFactors  =  FALSE
+                stringsAsFactors  =  TRUE
                 
                 
                 )
@@ -255,20 +255,20 @@ v2<-c("FirstText", "SecondText","ThirdText")
 v3<-c(1.2,4.4,7.4)
 
 #vector into a dataframe
-df2<-data.frame(col1=v1,col2=v2,col3=v3)
+df2<-data.frame(col1 = v1,col2 = v2,col3 = v3)
 df2
 
 
 
 #list to dataframe
-listdf<-list(item1=1:2,item2=c("SecondText","ThirdText"),item3=c(4.4,7.4))
+listdf<-list(item1 = 1:2,item2 = c("SecondText","ThirdText"),item3 = c(4.4,7.4))
 newldf<-as.data.frame(listdf)
 newldf
 
 #is.data.frame(newldf)
 
 #matrix to dataframe
-matA<-matrix(c(10,5,3,6),nrow = 2,ncol=2,byrow = TRUE,dimnames = list(c("m1R1","m1R2"),c("m1C1","m1C2")))
+matA<-matrix(c(10,5,3,6),nrow  =  2,ncol = 2,byrow  =  TRUE,dimnames  =  list(c("m1R1","m1R2"),c("m1C1","m1C2")))
 matdf<-as.data.frame(matA)
 matA
 
@@ -285,7 +285,7 @@ v<-c(1,2,4)
 df[,v]
 df[,2]
 df[,2]
-df[,2,drop =FALSE]
+df[,2,drop  = FALSE]
 
 mtcars
 mtcars[3]
@@ -293,37 +293,37 @@ mtcars[3]
 mtcars[10,]
 max(mtcars[1])
 
-a<-mtcars[mtcars$am==0,]
+a<-mtcars[mtcars$am =  = 0,]
 nrow(a)
-b<-mtcars[mtcars$vs==0,]
+b<-mtcars[mtcars$vs =  = 0,]
 row.names(b)
 
-sixCyllinder<-mtcars[mtcars$cyl==6,]
+sixCyllinder<-mtcars[mtcars$cyl =  = 6,]
 sixCyllinder
 
-foutcylinder<-mtcars[mtcars$cyl==4,]
+foutcylinder<-mtcars[mtcars$cyl =  = 4,]
 maxfcylmpg<-max(foutcylinder$mpg)
 maxfcylmpg
 
-nrow(foutcylinder[foutcylinder$am==0])
-foutcylinder[foutcylinder$am==0,]
+nrow(foutcylinder[foutcylinder$am =  = 0])
+foutcylinder[foutcylinder$am =  = 0,]
 
-mean(mtcars[(mtcars$cyl==8&mtcars$am==0),]$wt)
+mean(mtcars[(mtcars$cyl =  = 8&mtcars$am =  = 0),]$wt)
 
 
-#=========================================================
+# =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  = 
 student_scores<-data.frame(
-  StudentID= sample(1:100),
-  Gender=sample(c("Male","Female"),100,replace = TRUE),
-  Age= sample(18:25,100,replace = TRUE),
-  Marks=round(runif(100,min=40,max=99))
+  StudentID =  sample(1:100),
+  Gender = sample(c("Male","Female"),100,replace  =  TRUE),
+  Age =  sample(18:25,100,replace  =  TRUE),
+  Marks = round(runif(100,min = 40,max = 99))
 )
 
 print(student_scores)
 print(getwd())
 setwd("C:\\Users\\aravi\\Desktop\\StatisticsUsingR\\Classwork")
 print(getwd())
-write.csv(student_scores,"student_scores.csv",row.names = FALSE)
+write.csv(student_scores,"student_scores.csv",row.names  =  FALSE)
 data<-read.csv("student_scores.csv")
 print(data)
 max_marks<-max(data$Marks)
@@ -337,17 +337,17 @@ print(is.data.frame(data))
 age_greater_than_20<-data[data$Age>20,]
 age_greater_than_20
 
-female_with_mark_greater_than_50<-data[data$Gender=="Female"&data$Marks>50,]
+female_with_mark_greater_than_50<-data[data$Gender =  = "Female"&data$Marks>50,]
 female_with_mark_greater_than_50
 
-write.csv(female_with_mark_greater_than_50,"female>50Marks.csv",row.names = FALSE)
+write.csv(female_with_mark_greater_than_50,"female>50Marks.csv",row.names  =  FALSE)
 head(data)
 tail(data,10)
 
 #read a text file in R
-textfile <- readLines("sample.txt", encoding = "UTF-8") 
+textfile <- readLines("sample.txt", encoding  =  "UTF-8")
 textfile
-write.table(data,"sample.txt",sep="\t",quote=F,row.names = F)
+write.table(data,"sample.txt",sep = "\t",quote = F,row.names  =  F)
 x
 
 precip
@@ -356,14 +356,14 @@ discoveries
 
 
 #use stripchart for qualitative data
-stripchart(precip,xlab = "rainfall")
-stripchart(rivers,method = "jitter")
-stripchart(discoveries,method = "stack")
+stripchart(precip,xlab  =  "rainfall")
+stripchart(rivers,method  =  "jitter")
+stripchart(discoveries,method  =  "stack")
 
 #histogram
-hist(precip,main="")
-hist(precip,freq = TRUE,main = "Precip")
-hist(precip,freq = FALSE,breaks = 200)
+hist(precip,main = "")
+hist(precip,freq  =  TRUE,main  =  "Precip")
+hist(precip,freq  =  FALSE,breaks  =  200)
 
 
 carsdata<-as.data.frame(mtcars)
@@ -377,9 +377,9 @@ carsdata$vs<-as.factor(carsdata$vs)
 carsdata$cyl<-as.ordered(carsdata$cyl)
 carsdata$carb<-as.ordered(carsdata$carb)
 
-hist(carsdata$wt,xlab = "Car Weight",ylab = "Number of Cars",main = "Weight/Count Graph of Cars")
-barplot(height = table(carsdata$cyl))
-stripchart(carsdata$wt,xlab = "Car Weight")
+hist(carsdata$wt,xlab  =  "Car Weight",ylab  =  "Number of Cars",main  =  "Weight/Count Graph of Cars") # nolint
+barplot(height  =  table(carsdata$cyl))
+stripchart(carsdata$wt,xlab  =  "Car Weight")
 
 #-------------------------------------------------------
 #skewness 
@@ -398,25 +398,25 @@ rn
 density(rn)
 set.seed(3548145)
 x_norm<-rnorm(5000)
-hist(x_norm,prob=TRUE,ylim = c(0,0.5))
-lines(density(x_norm),col=2,lwd=3)
+hist(x_norm,prob = TRUE,ylim  =  c(0,0.5))
+lines(density(x_norm),col = 2,lwd = 3)
 
 
 #kurtosis
 #if gamma <3 it is platykurtic
-#if gamma = 3 it is mesokurtic
+#if gamma  =  3 it is mesokurtic
 #if gamma >3 it is leptokurtic
 
 kurtosis(x_norm)
-x<-c(rep(61,each=10),rep(64,each=18),
-     rep(65,each=23),rep(67,each=32),
-     rep(70,each=27),rep(73,each=17))
+x<-c(rep(61,each = 10),rep(64,each = 18),
+rep(65,each = 23),rep(67,each = 32),
+rep(70,each = 27),rep(73,each = 17))
 kurtosis(x)
 hist(x)
-lines(density(x),col=2,lwd=3)
+lines(density(x),col = 2,lwd = 3)
 
 
-rnorm_x<-rnorm(100)
+rnorm_x<-rnorm(100) # nolint
 kurtosis(meso_x)
 hist()
 
@@ -437,18 +437,18 @@ IQR(mt_data$qsec)
 hist(mt_data$qsec)
 library(ggplot2)
 den_qsec <- density(mt_data$qsec)
-plot(den_qsec, frame = FALSE, col = "blue",main = "Density plot")
+plot(den_qsec, frame  =  FALSE, col  =  "blue",main  =  "Density plot")
 
 var(mt_data$hp)
 sd(mt_data$hp)
 skewness(mt_data$disp)
 den_disp <- density(mt_data$disp)
-plot(den_disp, frame = FALSE, col = "blue",main = "Density plot")
+plot(den_disp, frame  =  FALSE, col  =  "blue",main  =  "Density plot")
 
 kurtosis(mt_data$drat)
 #it is platykurtic
 den_drat <- density(mt_data$drat)
-plot(den_drat, frame = FALSE, col = "blue",main = "Density plot")
+plot(den_drat, frame  =  FALSE, col  =  "blue",main  =  "Density plot")
 
 
 
@@ -465,4 +465,36 @@ summary(mt_data$mpg)
 median(mt_data$mpg)
 
 
-scatter.smooth(x=mt_data$hp,y=mt_data$qsec)
+scatter.smooth(x = mt_data$hp,y = mt_data$qsec)
+
+
+multivariate_data <- data.frame(Height  =  c(170, 175, 160, 185, 155), Weight  =  c(65, 70, 55, 75, 50))
+multivariate_data
+data <- c(10, 20, 25,78, 30, 40)
+mean_value <- mean(data)
+sd_value <- sd(data)
+hist(data)
+stem(data)
+frequency_table <- table(data)
+hist(data)
+mean_value <- mean(data)
+median_value <- median(data)
+mean_value
+median_value
+
+# Hypothesis testing
+capt_crisp<-data.frame(weight=c(15.5,16.2,16.1,15.8,15.6,16.0,15.8,15.9,16.2))
+x_bar<-mean(capt_crisp$weight)
+s_bar<-sd(capt_crisp$weight)
+mu_0<-16
+n<-length(capt_crisp$weight)
+#find the t value
+t_statistic<-(x_bar-mu_0)/(s_bar/sqrt(n))
+t_statistic
+
+?t.test()
+
+t.test(capt_crisp$weight,
+       alternative = c("two.sided", "less", "greater"),
+       mu = mu_0 ,paired = FALSE,
+       conf.level = 0.95)
